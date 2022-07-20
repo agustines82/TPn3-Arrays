2362ba480d450c463e782f6d84f61cf6c9cdfd06
Agustin Sampayo <agustines82@gmail.com>
2022-07-20 19:47:35 -0300

    pequeño arreglo en ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,7 +1,7 @@
 //Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 
 function multiplos(numero) {
-    for (multiplicador = 1; multiplicador <= 10; multiplicador++) {
+    for (let multiplicador = 1; multiplicador <= 10; multiplicador++) {
         let resultado = numero * multiplicador;
         document.write(`${numero} x ${multiplicador} = ${resultado} <br>`);
     }
